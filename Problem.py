cadaebaa057f95839d34d9e28dd2a610d80dd77a
Wolgan Ens <ens.wolgan@gmail.com>
2018-06-22 14:59:07 -0300

    Esqueleto inicial do problema

---
+++ Problem.py
@@ -0,0 +1,17 @@
+class Problem(object):
+	"""Classe para resolução do problema de clustering de Tweets"""
+	def __init__(self, d, p_size);
+		# Matriz de dissimilaridade
+		self.d = d
+		# Tamanho da população
+		self.p_size = p_size
+		# Lista de gerações
+		self.generations = list()	
+	
+	def random_start(self):
+		for i in range(p_size):
+			pass
+	
+	def fitness(self):
+		pass
+		
