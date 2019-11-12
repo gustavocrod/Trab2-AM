9caa79db0743aa2e1d018097153a5648e34a6db4
Gustavo C. Rodrigues <37716771+gustavocrod@users.noreply.github.com>
2019-11-12 12:37:25 -0300

    Update Individual.py

--- Individual.py
+++ Individual.py
@@ -2,7 +2,8 @@ class Individual():
 	"""
 		Um individuo eh uma matriz
 	"""
-	def __init__(self, membership_matrix):
-		self.fitness = 0.0 # Aptidao do moco no mundo a fora
+	def __init__(self, membership_matrix, generation):
+		self.fitness = 0.0000000 # Aptidao do moco no mundo a fora
 		self.membership_matrix = membership_matrix # membership de cada tweet (n_tweets x n_cluster)
 		self.id = 0
+		self.generation = generation
