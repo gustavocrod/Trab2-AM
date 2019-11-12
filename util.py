4fd5196397c7aae1767e6da81a5928174eda2985
Gustavo C. Rodrigues <37716771+gustavocrod@users.noreply.github.com>
2019-11-12 12:36:41 -0300

    Update util.py

--- util.py
+++ util.py
@@ -7,17 +7,16 @@ from Problem import *
 def levenshtein(a,b):
     """
     Calcula o numero de operações para transformar a em b
-
     :param a: primeira string
     :param b: segunda string
     :return: distancia de levenshtein
     """
+
     return edit_distance(a,b)
 
-def jaccard(a,b):
+def jaccard_velho(a,b):
     """
     Medida de similaridade baseada em quantos elementos dois conjuntos compartilham
-
     :param a: primeira string
     :param b: segunda string
     :return: retorna 0 caso ?
@@ -25,7 +24,6 @@ def jaccard(a,b):
     """
     a_words = set(a.split())
     b_words = set(b.split())
-
     numerator = 0
     
     if a_words > b_words:
@@ -37,13 +35,28 @@ def jaccard(a,b):
     
     for a in shorter:
         for b in greater:            
-            if levenshtein(a, b)/len(b if b > a else a) <= 0.4:
+            if levenshtein(a, b)/len(b if b > a else a) <= 0.3:
                 numerator += 1
     
-
-    if a_words.isdisjoint(b_words):
+    if numerator == 0:
         return 0
-    return 1 - (numerator / (len(a_words) + len(b_words) - numerator))
+    return 1 - (numerator) / (len(a_words) + len(b_words) - numerator)
+
+def jaccarddd(a,b):
+    """
+    Medida de similaridade baseada em quantos elementos dois conjuntos compartilham
+    :param a: primeira string
+    :param b: segunda string
+    :return: retorna 0 caso ?
+             retorna a medida de jaccard
+    """
+    a_words = set(a.split())
+    b_words = set(b.split())
+    
+    
+    if len(a_words.intersection(b_words)) == 0:
+        return 1.0
+    return 1 - len(a_words.intersection(b_words)) / len(a_words.union(b_words))
 
 def load_tweets():
     """
@@ -59,6 +72,17 @@ def load_tweets():
         print("Falha ao abrir arquivo json")
         raise e
 
+def print_population(p):
+    cont = 1
+    for ind in p.population:
+        print("=================")
+        print("membership matrix, geracao: {}, individuo: {}".format(p.generation, cont))
+        for row in ind.membership_matrix:
+            print(row)
+        print("Fitness", ind.fitness)
+        print("==================")
+        input()
+        cont += 1
 
 def remove_stop_words(tweets):
     """
